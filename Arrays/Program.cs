47fdb178a1a233c7b4dcaa25213f9f652c01959a
Delaney Green <delaney.rosa1@gmail.com>
2024-05-09 16:29:35 -0500

    commit whore

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,6 +1,12 @@
 ﻿//Exercises: Control Flow & Collections
 
 //TODO: Write your code for Array Practice below:
+Console.WriteLine("Hello, Arrays!");
+int[] numArr = {1,1,2,3,5,8};
+foreach(int n in numArr){
+    if(n%2 != 0){System.Console.WriteLine(n);}
+    
+}
 
 Console.WriteLine("Hello, Arrays!");
 

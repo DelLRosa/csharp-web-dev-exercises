cb9b08791beee5a3a43a44f7e1d92971bc5d323f
Delaney Green <delaney.rosa1@gmail.com>
2024-05-09 16:34:21 -0500

    commit

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -1,6 +1,9 @@
 ﻿//Exercises: Control Flow & Collections
-
+string str = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
 //TODO: Write your code for String Practice below:
+string[] strArr = str.Split('.');
+System.Console.WriteLine(string.Join('!',strArr));
+
 
 Console.WriteLine("Hello, Strings!");
 

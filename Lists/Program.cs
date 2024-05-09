1a6851db1230fd6fc1bee48c3a48c3eb24690753
Delaney Green <delaney.rosa1@gmail.com>
2024-05-09 16:55:14 -0500

    dhf

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -1,5 +1,29 @@
 ﻿//Exercises: Control Flow & Collections
 
 //TODO: Write your code for List Practice below:
+static int sumNum(List<int> arr){
+    int sum = 0;
+    foreach(int num in arr){
+        if(num%2==0){
+            sum+=num;
+        }
+    }
+    return sum;
+}
 
-Console.WriteLine("Hello, Lists!");
+static void printer(List<string> list){
+    System.Console.WriteLine("how many letters are we looking for? ");
+    int numLet=Convert.ToInt32(Console.ReadLine());
+    foreach(string word in list){
+        if (word.Length==numLet){
+            System.Console.WriteLine($"The word {word} has {numLet} letters");
+        }
+    }
+}
+
+List<string> wordList = new List<string>{"butt","woo","qw","Hello"};
+
+// List<int> numList = new List<int>{1,2,3,4,5,6,7,8,9,10};
+// Console.WriteLine("Hello, Lists!");
+// System.Console.WriteLine(sumNum(numList));
+printer(wordList);
